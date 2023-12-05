8c114a0dcd34a97aa6be234327fd3daeb50e0d7b
EzeOkK <135455528+EzeOkK@users.noreply.github.com>
2023-12-04 21:50:40 -0300

    Update example.py

--- example.py
+++ example.py
@@ -2,7 +2,10 @@ try:
   from pytools.organize import CodeOrganizer
 except:
   import os
-  os.system("pip install ")
+  os.system("pip install Pytools.api")
+
+
+
 file_path = "your/path/to/file.py"
 organizer = CodeOrganizer()
 organizer.organize_code_file(file_path)
