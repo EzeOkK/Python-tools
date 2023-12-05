d9f59b3619e4f216fab560ee431f1eb110d41c29
EzeOkK <135455528+EzeOkK@users.noreply.github.com>
2023-12-04 21:45:12 -0300

    Update Organize.py

--- Pytools/Organize.py
+++ Pytools/Organize.py
@@ -1,12 +1,14 @@
+# pytools/organize.py
+
 class CodeOrganizer:
     @staticmethod
     def organize_imports(file_content):
         """Organize the imports in a given python code content."""
-        
+
         lines = file_content.strip().split('\n')
         import_lines = sorted([line.strip() for line in lines if line.strip().startswith('import') or line.strip().startswith('from')])
         other_lines = [line.strip() for line in lines if not (line.strip().startswith('import') or line.strip().startswith('from'))]
-        
+
         organized_code = '\n'.join(import_lines) + ('\n\n' if import_lines else '') + '\n'.join(other_lines)
         return organized_code.strip()
 
@@ -39,13 +41,13 @@ class CodeOrganizer:
         with open(file_path, 'r') as file:
             content = file.read()
 
-        
+
         lines = content.split('\n')
         corrected_lines = [cls.correct_typo_errors(line) for line in lines]
         corrected_content = '\n'.join(corrected_lines)
 
-        
+
         organized_content = cls.organize_imports(corrected_content)
-        
+
         with open(file_path, 'w') as file:
             file.write(organized_content)
